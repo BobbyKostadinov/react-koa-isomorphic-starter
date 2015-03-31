e48a79e431f7fb74292c435cd42898d85750d117
Bobby Kostadinov <Bobby.Kostadinov@dowjones.com>
2015-03-31 13:08:29 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-# React Isomorphic Starter
+# React Koa Isomorphic Starter
 
 This is a starter project for an isomorphic React application.
-Eventually this will be a yeoman package.
+Inspired by (with some heavy borrowing) https://github.com/nemtsov/react-isomorphic-starter
 
 
 ## Usage
 
 Development:
+`npm install`
 `gulp dev`
 
 Production:
 `npm install`
-`npm start`
+`npm start --harmony`
+
+(Notice the harmony flag. For current Node version. 0.12.0 this is still required to run EC6 generators that KOA needs)
 
 
 ## License
--- server.js
+++ server.js
@@ -1,26 +1,70 @@
 require('node-jsx').install({extension: '.jsx'});
 
-var express = require('express'),
-  compression = require('compression'),
-  disableHttpCache = require('connect-cache-control'),
-  morgan = require('morgan'),
-  serveStatic = require('serve-static'),
-  errors = require('common-errors'),
-  PORT = process.env.PORT || 8000,
-  IS_PROD = 'production' === process.env.NODE_ENV,
-  app = module.exports = express();
+var compression = require('compression'),
+    morgan = require('morgan'),
+    serveStatic = require('koa-static'),
+    errors = require('common-errors'),
+    PORT = process.env.PORT || 8000,
+    IS_PROD = 'production' === process.env.NODE_ENV,
+    info = require('./package.json'),
+    mount = require('koa-mount');
+    React = require('react'),
+    koaBunyanLogger = require('koa-bunyan-logger'),
+    App = React.createFactory(require('./lib/components/App/App.jsx')),
+    router = require('koa-router')();
+    app = module.exports = require('koa')();
 
-// utility middleware
-app.use(disableHttpCache);
-app.use(morgan(IS_PROD ? 'combined' : 'dev'));
-app.use(compression());
-app.use('/favicon.ico', require('./lib/resources/favicon'));
-app.use('/_health', require('./lib/resources/_health'));
 
-// app resources
-app.use('/assets', serveStatic(__dirname + '/dist'));
-app.use('/', require('./lib/resources/pages'));
-app.use(errors.middleware.errorHandler);
+//Init logger
+app.use(koaBunyanLogger());
+app.use(koaBunyanLogger.requestIdContext());
+app.use(koaBunyanLogger.timeContext());
+
+app.use(function *(next) {
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  this.set('X-Response-Time', ms + 'ms');
+  this.log.info('%s %s for %s; duration: %sms', this.request.method, this.request.ip, this.path, ms);
+});
+
+//Set cache headers
+app.use(function *(next) {
+  this.set('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+  this.set('Cache-Control', 'no-store, ' + 'no-cache, must-revalidate, max-age=0');
+  this.set('Pragma', 'no-cache');
+  yield next;
+});
+
+
+app.use(function *(next) {
+   compression();
+   yield next;
+});
+
+router.get('/favicon.ico', function *(next) {
+  this.body = '';
+  yield next;
+});
+router.get('/_health', function *(next) {
+  this.body = {
+    status: 'ok',
+    name: info.name,
+    version: info.version
+  };
+  yield next;
+});
+router.get('/', function *(next) {
+  var props = {};
+  this.set('X-Request-Id', this.reqId);
+  this.body = '<!DOCTYPE html>' + React.renderToString(App(props));
+  yield next;
+});
+
+app
+  .use(router.routes());
+
+app.use(mount('/assets', serveStatic(__dirname + '/dist')));
 
 if (!module.parent) {
   app.listen(PORT, function () {
